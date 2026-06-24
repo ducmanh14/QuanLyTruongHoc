47b7266d1edd1d0cad7b40fe8d692ed8226df6f7
long <long@DESKTOP-829QUB7>
2026-06-24 17:09:03 +0700

    fix

--- SourceCode/QuanLyTruongHoc/Data/DatabaseHelper.cs
+++ SourceCode/QuanLyTruongHoc/Data/DatabaseHelper.cs
@@ -4,7 +4,7 @@ namespace QuanLyTruongHoc.Data
 {
     public class DatabaseHelper
     {
-        // Đã thêm chữ DB vào sau QuanLyTruongHoc
+        
         private static string connectionString = @"Server=.\SQLEXPRESS;Database=QuanLyTruongHocDB;Integrated Security=True;TrustServerCertificate=True;";
 
         public static SqlConnection GetConnection()
